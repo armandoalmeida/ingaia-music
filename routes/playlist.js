4402c1ad4d3ea38b192c2da7642cb492b201e0ca
Jose Armando Almeida <jose.almeida@softvaro.com.br>
2019-06-17 01:46:27 -0300

    Finalização da implementação

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const router = express.Router();
-const request = require("request");
+const httpStatus = require('http-status-codes');
 
-const weatherAppId = "012fb3d2351e593e6d57e41611ad97f0";
+const weather = require("../lib/weather");
+const spotify = require("../lib/spotify");
+
+const router = express.Router();
 
 router.get('/:city', (req, res, next) => {
-
-  const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&units=metric&APPID=${weatherAppId}`;
-
-  request.get(weatherURL, (error, response, body) => {
-    if (error) {
-      return console.error(error);
-    }
-    res.json(JSON.parse(body))
-  });
-
-});
+    
+    weather(req.params.city).then(w => {
+        spotify(w.genre).then(tracks => {
+            res.json({
+                cod: httpStatus.OK,
+                ...w,
+                tracks: tracks
+            });
+        });
+    }, reason => res.json(reason));
+}); 
 
 module.exports = router;
